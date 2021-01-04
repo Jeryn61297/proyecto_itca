0ac527997ad6158cb32cb862d8ac9c5ce5a4cd06
Jeryn61297 <jesus_karyn.61297@yahoo.com>
2021-01-04 01:03:28 -0600

    cree calendario

--- lib/src/home_pages/home_page.dart
+++ lib/src/home_pages/home_page.dart
@@ -1,5 +1,6 @@
 //import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:proyecto_itca/src/pag/calendario.dart';
 import 'package:proyecto_itca/src/pag/croquis.dart';
 
 import 'package:proyecto_itca/src/pag/fold_carreras/carreras.dart';
@@ -266,7 +267,13 @@ class HomePage extends StatelessWidget {
             trailing: Icon(Icons.keyboard_arrow_right),
             title: Text('Calendario escolar'),
             subtitle: Text(' '),
-            onTap: () {},
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (BuildContext context) => CalendarioPage()));
+            },
           ),
           Divider(),
           //----------------------------------------------
---
+++ lib/src/pag/calendario.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CalendarioPage extends StatelessWidget {
+  static final String routeName = 'calendario';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendario escolar'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 0),
+            child: Column(
+              children: <Widget>[
+                _Titulo(),
+                _Image1(),
+                _Image2(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget _Titulo() {
+  return Container(
+    alignment: Alignment.center,
+    width: double.infinity,
+    height: 60,
+    //color: Colors.red,
+    child: Text(
+      'Calendario',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+    ),
+  );
+}
+
+Widget _Image1() {
+  return Container(
+    width: double.infinity,
+    height: 200,
+    color: Colors.blue,
+    child: Image(
+      image: AssetImage('assets/images/5.jpg'),
+    ),
+  );
+}
+
+Widget _Image2() {
+  return Container(
+    width: double.infinity,
+    height: 180,
+    color: Colors.yellow,
+    child: Image(
+      image: AssetImage('assets/images/1000.jpg'),
+    ),
+  );
+}
