671067916db54429b5c911f983af42f9c56dfdba
Jeryn61297 <jesus_karyn.61297@yahoo.com>
2021-01-10 13:59:15 -0600

    construccion de la pagina informatica

--- lib/src/pag/fold_carreras/informatica.dart
+++ lib/src/pag/fold_carreras/informatica.dart
@@ -16,6 +16,9 @@ class InformaticaPage extends StatelessWidget {
               children: <Widget>[
                 SizedBox(height: 2),
                 _TituloPage(),
+                _Objetivo(),
+                SizedBox(height: 9),
+                _Semestres(),
               ],
             ),
           )
@@ -37,5 +40,88 @@ Widget _TituloPage() {
           color: Colors.black, fontSize: 24, fontWeight: FontWeight.w500),
       textAlign: TextAlign.center,
     ),
+  );
+}
+
+Widget _Objetivo() {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 10),
+    child: Container(
+      width: double.infinity,
+      height: 230,
+      //color: Colors.yellowAccent,
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 10),
+          Text(
+            'Objetivo General',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 22,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.start,
+          ),
+          Text(
+            'Formar profesionales competentes en el diseño, desarrollo, implmentación y administración de proyectos informáticos con una visión sistématica, tecnológica y estratégica; ofreciendo soluciones innovadoras e integrales a las organizaciones de acuerdo con las necesidades actuales; comprometidos con su entorno, desempeñandose con actitud ética, emprendedora y de liderazgo.',
+            style: TextStyle(color: Colors.black, fontSize: 15),
+            textAlign: TextAlign.justify,
+          ),
+          SizedBox(height: 22),
+          Text(
+            'Plan de estudio',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+            ),
+            textAlign: TextAlign.start,
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _Semestres() {
+  return SafeArea(
+    child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        width: double.infinity,
+        height: 300,
+        color: Colors.grey,
+        child: Row(children: <Widget>[
+          Column(
+            children: [
+              Text(
+                'SEMESTRE 1',
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500),
+              ),
+              Text(
+                '> Administración para Informática.\n> Fundamentos de investigación.\n> Fundamentos de programación.\n> Taller de ética.\n> Cálculo diferencial.\n> Desarrollo sustentable. ',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 14),
+              Text(
+                'SEMESTRE 2',
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500),
+              ),
+              Text(
+                '> Administración para Informática.\n> Fundamentos de investigación.\n> Fundamentos de programación.\n> Taller de ética.\n> Cálculo diferencial.\n> Desarrollo sustentable. ',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ])),
   );
 }
